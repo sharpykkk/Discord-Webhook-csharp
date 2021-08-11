9ad4d73972572bd8b8490d31edf2d73d08c22a95
deep1dark <79944708+deep1dark@users.noreply.github.com>
2021-08-11 22:59:17 +0300

    Add files via upload

---
+++ DiscordWebhook/DiscordWebhook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Management;
+
+namespace DiscordWebhook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WeebHook dcWeb = new WeebHook())
+            {
+                ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+                foreach (ManagementObject managementObject in mos.Get())
+                {
+                    String OSName = managementObject["Caption"].ToString();
+                    dcWeb.ProfilePicture = ""; //url profile picture
+                    dcWeb.UserName = ""; //webhook username 
+                    dcWeb.WebHook = ""; //webhook url
+                    dcWeb.SendMessage("Hello World!"); //webhook message
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
